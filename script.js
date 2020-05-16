a9278660b6fb3e7ed67975a280d45a97e8b4141e
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-05-16 16:39:40 -0500

    the city name is on display

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 
-// displayMovieInfo function re-renders the HTML to display the appropriate content
-var inputCities = [];
+var inputCitiesArray = [];
 
 function displayCityInfo(cityInput) {
   var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=6f878781f6244ccdbc4b04689e3394dd";
-
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
+    console.log(response);
 
-
-    var cityName = $("<h1>").text(response.city.name + "DATE");
-    var tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
-    var cityTemp = $("<div").text("Temperature: " + tempF+ "F");
-    var windSpeed = $("<div").text("Wind Speed: " + response.list[0].wind.speed);
+    $("#display").empty();
+    var cityName = $("<h2>").text(response.city.name + " DATE");
+    //var tempF = ((response.main.temp - 273.15) * 1.80 + 32);
+    //var cityTemp = $("<div").text("Temperature: " + tempF+ "F");
+    var windSpeed = $("<p>").text("Wind Speed: " + response.list[0],wind.speed);
     //var cityTemp = $("<a>").attr("href", response.url).append(artistName);
-    console.log(cityName);
-    console.log(tempF);
-    console.log(cityTemp);
-    console.log(windSpeed);
+    //$("#display").append(cityTemp);
+    
+    //$("#display").append(windSpeed);
+    
+    $("#display").append(cityName, windSpeed);
+
 
 
 
@@ -27,10 +28,6 @@ function displayCityInfo(cityInput) {
     //var trackerCount = $("<h2>").text(response.tracker_count + " fans tracking this artist");
     //var upcomingEvents = $("<h2>").text(response.upcoming_event_count + " upcoming events");
     //var goToArtist = $("<a>").attr("href", response.url).text("See Tour Dates");
-
-    // Empty the contents of the artist-div, append the new artist content
-    //$("#display").empty();
-    $("#display").append(cityName, cityTemp, windSpeed);
 
 
     renderCityInfo();
@@ -41,35 +38,33 @@ function displayCityInfo(cityInput) {
 function renderCityInfo() {
     $("#city-list").empty();
 
-    for (var i = 0; i < inputCities.length; i++) {
+    for (var i = 0; i < inputCitiesArray.length; i++) {
         var aside = $("<aside>");
         aside.addClass("col text-dark bg-white border border-secondary rounded-sm city");
-        aside.attr("data-name", inputCities[i]);
-        aside.text(inputCities[i]);
+        aside.attr("data-name", inputCitiesArray[i]);
+        aside.text(inputCitiesArray[i]);
         $("#city-list").append(aside);
     }
-    
-
 }
 
-// This function handles events where the add movie button is clicked
+// This function handles the on click event when the search icon is clicked
 $("#city-submit").on("click", function (event) {
-  event.preventDefault();
-  // This line of code will grab the input from the textbox
-  var cityInput = $("#cityInput").val().trim();
-  //console.log($("#cityInput.value"));
-  inputCities.push(cityInput);
-  //console.log(cityInput);
+//prevents the page from refreshikng when a button is clicked  
+event.preventDefault();
 
-  displayCityInfo(cityInput);  
-
-  
+// This line of code will grab the input from the textbox
+    var cityInput = $("#cityInput").val().trim();
+//This takes the input from the form and addes it to an array that will be displayed
+    inputCitiesArray.push(cityInput);
+//after the data from the form is transfered into a usable object, the data is passed to the ajax call. 
+    displayCityInfo(cityInput);  
+//calling the function that wil create the aside elements
+    renderCityInfo(cityInput);
 
   // Calling renderButtons which handles the processing of our movie array
   //renderButtons();
 });
-//$(document).on("click", ".city", displayCityInfo);
-
-//renderCityInfo();
+//This should turn the displayed array of city's into buttons that can be re-called with the ajax function 
+$(document).on("click", ".city", displayCityInfo);
 
 //renderButtons();
